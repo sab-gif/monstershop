30b2d21568444518584b179c283dc04c543d819c
saba <sabaurrehman11@gmail.com>
2025-07-17 02:36:13 +0200

    feat(user): add User model, repository and required dependencies
    - Add User entity with JPA annotations for database persistence.
    - Implement UserRepository interface extending JpaRepository.
    - Add required Spring dependencies in pom.xml.
    - User service and controller to be implemented in follow-up commits.
    
    Initial setup for user management in Monster Shop backend.

---
+++ src/main/java/com/monstershop/backend/model/User.java
@@ -0,0 +1,65 @@
+package com.monstershop.backend.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(max = 50, message = "Username cannot exceed 50 character")
+    private String username;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+
+    //reseña del usuario: Como un usuario puede dejar varias reseñas
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    //Getters y Setter
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/monstershop/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.monstershop.backend.repository;
+
+import com.monstershop.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+}
