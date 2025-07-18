adc4c2ea692efb60aa3b2dfdc994bcd0d7cc1fd2
saba <sabaurrehman11@gmail.com>
2025-07-19 01:08:07 +0200

    feat(ErrorResponse): Add standardized error response model - Introduces ErrorResponse class for consistent error messaging in API. - Includes message and formatted timestamp for error tracking. - Supports structured error reporting to clients.

--- src/main/java/com/monstershop/backend/exception/ErrorResponse.java
+++ src/main/java/com/monstershop/backend/exception/ErrorResponse.java
@@ -1,4 +1,32 @@
 package com.monstershop.backend.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
 public class ErrorResponse {
+    String message;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    LocalDateTime timestamp;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
